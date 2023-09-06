2885e46e0d4f5ebbe27e086290b9b9c155097c8b
nikita.smirnov <nikita.smirnov@exactprosystems.com>
2023-09-06 16:59:46 +0400

    [TS-1663] Updated libraries

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#   Copyright 2020-2021 Exactpro (Exactpro Systems Limited)
+#   Copyright 2020-2023 Exactpro (Exactpro Systems Limited)
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -37,7 +37,7 @@ class ProtoGenerator(Command):
 
     def run(self):
         proto_path = os.path.abspath('src/main/proto')
-        gen_path = os.path.abspath('src/gen/main/python')
+        gen_path = os.path.abspath('build/generated/source/proto/main/services/python')
 
         if not os.path.exists(gen_path):
             os.makedirs(gen_path)
@@ -70,8 +70,8 @@ class CustomDist(sdist):
     def run(self):
         copy_tree(f'src/main/proto/{package_name}', package_name)
 
-        copy_tree(f'src/gen/main/python/{package_name}', package_name)
-        copy_tree(f'src/gen/main/services/python/{package_name}', package_name)
+        copy_tree(f'build/generated/source/proto/main/services/python/{package_name}', package_name)
+
         Path(f'{package_name}/__init__.py').touch()
 
         def make_packages(root_dir):
@@ -115,7 +115,8 @@ setup(
     license='Apache License 2.0',
     python_requires='>=3.7',
     install_requires=[
-        'th2-grpc-common~=3.1.2'
+        'th2-grpc-common==4.3.0.dev0',
+        'mypy-protobuf==3.4'
     ],
     packages=packages,
     package_data=package_data,
